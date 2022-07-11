1afa8e21811fcbad3f22dbb4350d72bf9a6d252a
Dmytro Lemeshko <Dmytro.Lemeshko@evolve-consultants.co.uk>
2022-07-11 21:05:30 +0300

    MONS-899-Refactor-xero-workers

--- src/Monspire.Xero.Lambdas.Contracts/ReconcileContactsInstruction.cs
+++ src/Monspire.Xero.Lambdas.Contracts/ReconcileContactsInstruction.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace Monspire.Xero.Lambdas.Contracts
 {
     public class ReconcileContactsInstruction
     {
         public int TenantId { get; set; }
+
+        public DateTime? ModifiedAfter { get; set; }
     }
 }
